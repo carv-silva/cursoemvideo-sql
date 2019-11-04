24e4d621e27af51a5938e2cb3bf9f91402cd83d8
Carlos VInicius <car_los.vinicius@hotmail.com>
2019-11-04 02:08:24 -0300

    Exerc-12-todos
    Exercicios da aula 12 corrigidos

--- comandosSql.sql
+++ comandosSql.sql
@@ -67,16 +67,16 @@ select * from  teste;
 select * from  cursos;
 
 insert into cursos values
-('1','HTML5','Curso de HTML5','40','37','2014'),
-('2','Algoritmos','Logica de Programação','20','15','2014'),
-('3','Photoshop','Dicas de Photoshop CC','10','8','2014'),
-('4','PGP','Curso de PHP para iniciantes','40','20','2010'),
-('5','Jarva','Introdução a Linguagem Java','10','29','2000'),
-('6','MySQL','Bancos de Dados MySQL','30','15','2016'),
-('7','Word','Curso completo de Word','40','30','2016'),
-('8','Sapateado','Danças Ritimicas','40','30','2018'),
-('9','Cozinha Arabe','Aprenda a fazer Kibe','40','30','2018'),
-('10','YouTuber','Gerar polemica e ganhar inscritos','5','2','2018');
+('HTML5','Curso de HTML5','40','37','2014'),
+('Algoritmos','Logica de Programação','20','15','2014'),
+('Photoshop','Dicas de Photoshop CC','10','8','2014'),
+('PGP','Curso de PHP para iniciantes','40','20','2010'),
+('Jarva','Introdução a Linguagem Java','10','29','2000'),
+('MySQL','Bancos de Dados MySQL','30','15','2016'),
+('Word','Curso completo de Word','40','30','2016'),
+('Sapateado','Danças Ritimicas','40','30','2018'),
+('Cozinha Arabe','Aprenda a fazer Kibe','40','30','2018'),
+('YouTuber','Gerar polemica e ganhar inscritos','5','2','2018');
 
 select * from cursos;
 desc cursos;
@@ -116,7 +116,7 @@ show tables; -- mostra as tabelas
 
 select * from cursos;
 
-///aula 10 phpmyadmin/// 
+///aula 10 phpmyadmin///
 
 create table amigos (
 	id int not null auto_increment,
@@ -129,8 +129,8 @@ create table amigos (
 alter table amigos
 add sexo enum('M','F')not null after nome;
 
-insert into amigos 
-(id, nome, sexo, telefone) 
+insert into amigos
+(id, nome, sexo, telefone)
 values (NULL, 'Maria','F','2222-3333'), (NULL, 'Joao','M', '2222-3333'),
 (NULL, 'Jose','M','3333-2222'),(NULL, 'Ana','F','1111-2222');
 
@@ -141,12 +141,12 @@ where id = '2';
 ///aula 11 select///
 
 select * from cursos
-order  by nome; --> seleciona todos os dado da coluna nome por ordem alfabetica.  
+order  by nome; --> seleciona todos os dado da coluna nome por ordem alfabetica.
 
 desc (nome da table) --->  mostra a tabela
 
 select * from cursos
-order  by nome desc(decrescente); --> aqui o desc serve para crescente inverter ordem alfabetica z-a 
+order  by nome desc(decrescente); --> aqui o desc serve para crescente inverter ordem alfabetica z-a
 
 select nome, carga, ano from cursos
 order by ano, nome; --> aqui ele vai ordenar em forma crescente o ano dps o nome
@@ -156,23 +156,23 @@ where ano = '2016' -->  mostra na tabela somente cursos quem tem ano de 2016.
 order by 'nome';
 
 select nome, carga from cursos
-where ano = '2016' --> nessa consulta ele nao mostra o campo 2016, 
+where ano = '2016' --> nessa consulta ele nao mostra o campo 2016,
 order by 'nome'; -->pois ele ja tem conhecimento que todos os cursos que vc pesquisou sao de 2016
 
 result set --> nome tecnico para os dados que retorna de uma consulta
 
-query --> é um pedido de uma informação ou de um dado. 
-Esse pedido também pode ser entendido como uma consulta, uma solicitação ou, ainda, uma requisição. 
+query --> é um pedido de uma informação ou de um dado.
+Esse pedido também pode ser entendido como uma consulta, uma solicitação ou, ainda, uma requisição.
 
 // operadores relacionais//
 
 select nome, descricao from cursos
-where ano <= '2015' --> aqui ele vai selecionar todos os cursos menor ou igual a 2015 
+where ano <= '2015' --> aqui ele vai selecionar todos os cursos menor ou igual a 2015
 order by nome; -->no mesmo sentindo que nao vai aparecer a coluna de 2015 e 2014 pq ele ja sabe
 
 select nome, ano from cursos
 where ano between '2014' and '2016' --> serve para selecionar dados entre ex: "anos" and "anos"
-order by ano desc, nome asc; --> order by significa ordenação entre dados	
+order by ano desc, nome asc; --> order by significa ordenação entre dados
 
 select nome, descricao, ano from cursos
 where ano in (2014, 2016) --> aqui ele vai mostra a saida de dados entre 2014 e 2016
@@ -180,12 +180,12 @@ order by ano, nome; --> ordenando em ano e dps nome order alfabetica e dps numer
 
 select * from cursos
 where carga > 35 and totaulas < 30
-order by ano, nome; 
+order by ano, nome;
 
 select nome, carga, totaulas from cursos
 where carga > 35 and totaulas < 30; --> aqui ele vai retorna os dados somente qnd tiver
 										 carga maior que 35 e totalaulas menos que 30.
- 
+
 select nome, carga, totaulas from cursos
 where carga > 35 or totaulas < 30; --> aqui ele vai retorna os dados somente qnd tiver
 									  valor de carga maior que 35 ou totalaulas menor que 30 (um ou outro).
@@ -193,61 +193,61 @@ where carga > 35 or totaulas < 30; --> aqui ele vai retorna os dados somente qnd
 --------------####------------------Curso MySQL #12 - SELECT (Parte 2)--------------####------------------
 
 LIKE --> significa parecido geralmente é usando junto com % que significa nenhum ou varios caracteres
-ex: 
+ex:
 select * from cursos
-where nome like 'p%'; --> aqui ele ira retorna a consulta como todos nomes que começa com p 
-que no caso o % ele representa qualquer conjunto de caractere que vem pela frente 
-obs: precisa do % e ele é sensitive case , e se vc colocar o % no começo da palvra ele busca os 
+where nome like 'p%'; --> aqui ele ira retorna a consulta como todos nomes que começa com p
+que no caso o % ele representa qualquer conjunto de caractere que vem pela frente
+obs: precisa do % e ele é sensitive case , e se vc colocar o % no começo da palvra ele busca os
 nome que tenha a letra so no fina da palavra
 
 select * from cursos
 where nome like '%a%'; --> aqui ele vai retorna qualquer nome que teja a palavra a por causa do %
 
 select * from cursos
-where nome not like '%a%'; --> vai retorna nome sem a letra 
+where nome not like '%a%'; --> vai retorna nome sem a letra
 
 select * from cursos
 where nome not like '%a%';
 
-update cursos set nome = 'Poo' 
-where idcurso = '9';  --> muda o valor do nome para Poo atraves do id 
+update cursos set nome = 'Poo'
+where idcurso = '9';  --> muda o valor do nome para Poo atraves do id
 
-select * from  cursos 
-where nome like 'ph%p'; --> retorna todos os nome que tenha ph no comeco 
+select * from  cursos
+where nome like 'ph%p'; --> retorna todos os nome que tenha ph no comeco
 
-select * from  cursos 
+select * from  cursos
 where nome like 'ph%p%'; --> retorna todos curso  que tenha ph no comeco e tenha algum dado dps do p ex: php7
- 
-select * from  cursos 
+
+select * from  cursos
 where nome like 'ph%p_'; --> retorno todos os cursos com nome que comeca com ph termina com p e tenha alguma coisa no final
 
 
 
-select * from  gafanhotos 
+select * from  gafanhotos
 where nome like '%silva%'; --> retorna todos os nomes que tenha silva ou que comece
 
-select * from  gafanhotos 
+select * from  gafanhotos
 where nome like '%_silva%'; --. retorna todos os nome que tenha sobrenome silva
 
-select * from  gafanhotos 
+select * from  gafanhotos
 where nome like '%silva'; --> termina com silva
 
-select * from  gafanhotos 
+select * from  gafanhotos
 where nome like 'silva%'; -- comeca com silva
 
 select  nacionalidade from gafanhotos; --> retorna todas nacionalidades da tabela
 
-select distinct carga from cursos; --> vai retorna so carga que nao repete valores iguais 
+select distinct carga from cursos; --> vai retorna so carga que nao repete valores iguais
 
 select distinct nacionalidade from gafanhotos  --> retorna todas nacionalidades sem repedicao de valores
 order by nacionalidade; --> retorna em ordem alfabetica
 
- select carga from cursos 
+ select carga from cursos
  order by carga; --> retorna   todas horario de carga da tabela
 
 
 select distinct carga from cursos
-order by carga; --> retorna todas os horarios de carga sem repeticao 
+order by carga; --> retorna todas os horarios de carga sem repeticao
 
 
 --- agregacao ---
@@ -264,13 +264,13 @@ select count(*) from cursos where carga > 30;
 
 select max(carga) from cursos; --> retorna o resultado da maior carga da tabela
 
-select max(totaulas) from cursos where ano = '2016'; --> dentro do curso de 2016 retorna o max numero de aulas 
+select max(totaulas) from cursos where ano = '2016'; --> dentro do curso de 2016 retorna o max numero de aulas
 
 select min(totaulas) from cursos; --> retorna o min de aulas da tabela cursos
 
 select nome, min(totaulas) from cursos where ano = '2016'; --> nao ta funcionando nao sei pq
 
-select sum(totaulas) from cursos; --> retorna a soma do total de aulas 
+select sum(totaulas) from cursos; --> retorna a soma do total de aulas
 
 select sum(totaulas) from cursos where ano = '2016'; --> retorna a soma do total de aulas dos cursos onde o ano é 2016
 
@@ -284,18 +284,44 @@ select avg(totaulas) from cursos where ano = '2016'; --> retorna a media do tota
 1 - 'Uma lista com o nome de todas as gafanhotas'
 2 - 'Uma lista com os dados de todas aquelas que nasceram entre 1/Jan/2000 e 31/Dez/2015'
 3 - 'Uma lista com o nome de todos os homens que trabalaham como Programadores'
-4 - 'Uma lista com os dados de todas as mulheres que nasceram no Brasil e que tem seu nome iniciando com a letra J'
-5 - 'Uma lista com o nome e nacionalidade de todos os homens que tem Silva no nome, nao nasceram no Brasil e pesam menos de 100 kg'
+4 - 'Uma lista com os dados de todas as mulheres que nasceram no Brasil e que
+tem seu nome iniciando com a letra J'
+5 - 'Uma lista com o nome e nacionalidade de todos os homens que tem Silva no
+nome, nao nasceram no Brasil e pesam menos de 100 kg'
 6 - 'Qual e a maior altura entre gafanhotos homens que moram no brasil'
 7 - 'Qual e a media de peso dos gafanhotos cadastrados?'
-8 - ''
+8 - 'Qual e o menor peso entre as gafanhotas mulheres que nasceram foram do
+brasil e entre 01/jan/1990 e 31/dez/2000'
+9 - 'Quantas gafanhotas mulheres tem mais de 1.90m de altura?'
+
+1 - select * from gafanhotos
+where sexo = 'F';
+
+2 - select * from gafanhotos
+where nascimento between '2000-01-01' and '2015-12-31'
+order by nascimento;
+
+3 - select nome, sexo from gafanhotos
+where sexo = 'M' and profissao = 'Programador'
+order by nome;
+
+4 - select nome, sexo,nacionalidade from gafanhotos
+where nome like 'j%' and sexo = 'F' and nacionalidade = 'Brasil'
+order by nome;
 
 
+5 - select nome, nacionalidade from gafanhotos
+where sexo = 'M' and nome like '%Silva%' and nacionalidade != 'Brasil'
+and peso < "100";
 
+6 - select max(altura) from gafanhotos
+where sexo = 'M' and nacionalidade = 'Brasil';
 
+7 - select avg(peso) from gafanhotos;
 
+8 - select min(peso) from gafanhotos
+where sexo ='F' and nacionalidade != 'Brasil'
+and nascimento between '1990-01-01' and '2000-12-31';
 
-
-
-
-
+9 - select count(nome) from gafanhotos
+where sexo = 'F' and altura > 1.90;
